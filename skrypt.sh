edebd461f647b498eb4b15d057389c00491eacd9
Tirgoth <karol1111.70@gmail.com>
2022-07-15 18:08:21 +0200

    log30

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-nazwa_pliku=$(basename -- "$0")
-
+argument2=$2 
 data(){
 	
 	date
@@ -10,10 +9,17 @@ data(){
 
 logs(){
 
-	for i in {1..100}
+	ilosc=100
+	
+	if [ "$argument2" -gt 0 ] 
+	then ilosc=$argument2
+	fi
+
+	for ((i=1;i<=ilosc;i++))
 	do
 		echo log$i $(basename $0) $(data) > logs/log$i.txt
 	done
+	
 }
 
 
