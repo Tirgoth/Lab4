da55d06a97e55935c1fe30abbe2f065a47d866c3
Tirgoth <karol1111.70@gmail.com>
2022-07-15 17:30:59 +0200

    logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-date(){
+nazwa_pliku=$(basename -- "$0")
+
+data(){
 	
 	date
 
 }
 
+logs(){
+
+	for i in {1..100}
+	do
+		echo log$i $(basename $0) $(data) > logs/log$i.txt
+	done
+}
 
 
-
-if [ "$1" = "--date" ]
+if [ "$1" = "--data" ]
 then
-	Date
+	data
+elif [ "$1" = "--logs" ]
+then
+	logs
 fi
 
